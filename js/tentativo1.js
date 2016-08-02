2b51527736d1f892391feba9cccd7c6c1e6b0c57
GiuseppeCuce <giuseppecuce.og@gmail.com>
2016-08-02 16:12:27 +0300

    index e directive

--- js/tentativo1.js
+++ js/tentativo1.js
@@ -1,6 +1,5 @@
 app.factory('tentativo1', ['$http', function($http) {
-	return 
-	$http.get('https://raw.githubusercontent.com/johnpapa/ng-demos/master/grunt-gulp/data/thor.json')
+	return $http.get('https://raw.githubusercontent.com/johnpapa/ng-demos/master/grunt-gulp/data/thor.json')
 	.success(function(data) {
 		return data;
 	}).error(function(err) {
